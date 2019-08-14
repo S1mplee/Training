2e53b612147297fe9132281c2d32e96059dc3184
S1mplee <boumellessa.ali@gmail.com>
2019-08-14 13:03:21 +0100

    Adding Sale Class with SaleAchieved Event

---
+++ Exercice_Eventstore/Eventstore/Eventstore/Sale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventstore
+{
+    class Sale
+    {
+        private Guid _Id;
+        private string _ProductName;
+        private int _qts;
+        private decimal _price;
+
+        private Sale(Guid id, string name, int q, decimal p)
+        {
+            this._Id = id;
+            this._ProductName = name;
+            this._qts = q;
+            this._price = p;
+        }
+
+    }
+}
---
+++ Exercice_Eventstore/Eventstore/Eventstore/SaleAchieved.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventstore
+{
+    public class SaleAchieved
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public int Qts { get; set; }
+        public decimal Price { get; set; }
+
+        public SaleAchieved(Guid id, string name, int q, decimal p)
+        {
+            this.Id = id;
+            this.ProductName = name;
+            this.Qts = q;
+            this.Price = p;
+        }
+
+        public override string ToString()
+        {
+            return "" + Id + " " + " " + ProductName + " " + Price + " " + Qts;
+        }
+    }
+}
