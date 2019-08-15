519266b5c4535613d182cd029aa8ba7b00986145
S1mplee <boumellessa.ali@gmail.com>
2019-08-15 09:53:17 +0100

    adding commands

---
+++ Exercice_DDD/DDD/DDD/Commands.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Command
+{
+
+}
+
+public class ChequeDepose : Command
+{
+    public readonly Guid accountId;
+    public readonly decimal Amount;
+
+    public ChequeDepose(Guid id,decimal amount)
+    {
+        this.accountId = id;
+        this.Amount = amount;
+    }
+}
+
+public class CashDepose : Command
+{
+    public readonly Guid accountId;
+    public readonly decimal Amount;
+
+    public CashDepose(Guid id, decimal amount)
+    {
+        this.accountId = id;
+        this.Amount = amount;
+    }
+}
+
+public class CashWithdraw : Command
+{
+    public readonly Guid accountId;
+    public readonly decimal Amount;
+
+    public CashWithdraw(Guid id,decimal amount)
+    {
+        this.accountId = id;
+        this.Amount = amount;
+    }
+}
+
+public class CashTransfer : Command
+{
+    public readonly Guid accountId;
+    public readonly Guid receiverId;
+    public readonly decimal Amount;
+
+    public CashTransfer(Guid id,Guid id2,decimal d)
+    {
+        this.accountId = id;
+        this.receiverId = id2;
+        this.Amount = d;
+    }
+
+
+}
+
+
+
---
+++ Exercice_DDD/DDD/DDD/DomainModel/AccountBalance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.DomainModel
+{
+    public class AccountBalance
+    {
+        private Guid Id;
+        private string _holderName;
+        private decimal _overdraftLimit;
+        private decimal _wireTransertLimit;
+        private bool _blocked;
+    }
+}
