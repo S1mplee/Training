773a3b505adaf659e01b615dd7e58b07386a4140
S1mplee <boumellessa.ali@gmail.com>
2019-08-15 10:02:35 +0100

    Adding Events

--- Exercice_DDD/DDD/DDD/Events.cs
+++ Exercice_DDD/DDD/DDD/Events.cs
@@ -9,3 +9,72 @@ public class Event
 {
 
 }
+
+public class CashDeposed : Event
+{
+    public readonly Guid AccountId;
+    public readonly decimal Amount;
+
+    public CashDeposed(Guid id,decimal a)
+    {
+        this.AccountId = id;
+        this.Amount = a;
+    }
+}
+
+public class ChequeDeposed : Event
+{
+    public readonly Guid AccountId;
+    public readonly decimal Amount;
+
+    public ChequeDeposed(Guid id, decimal a)
+    {
+        this.AccountId = id;
+        this.Amount = a;
+    }
+}
+
+public class CashWithdrawn : Event
+{
+    public readonly Guid AccountId;
+    public readonly decimal Amount;
+
+    public CashWithdrawn(Guid id, decimal a)
+    {
+        this.AccountId = id;
+        this.Amount = a;
+    }
+}
+
+public class AccountBlocked : Event
+{
+    public readonly Guid AccountId;
+    public AccountBlocked(Guid id)
+    {
+        this.AccountId = id;
+    }
+}
+
+public class AccountUnBlocked : Event
+{
+    public readonly Guid AccountId;
+    public AccountUnBlocked(Guid id)
+    {
+        this.AccountId = id;
+    }
+}
+
+public class CashTransfered : Event
+{
+    public readonly Guid AccountId;
+    public readonly Guid receiverId;
+    public readonly decimal Amount;
+
+    public CashTransfered(Guid id,Guid id2,decimal d)
+    {
+        this.AccountId = id;
+        this.receiverId = id2;
+        this.Amount = d;
+    }
+}
+
