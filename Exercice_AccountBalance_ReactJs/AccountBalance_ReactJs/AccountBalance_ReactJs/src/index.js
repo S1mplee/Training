91fc696eff309ac6bc0b8ce82cb2382c0fbb60c3
Ali Boumellessa <boumellessa.ali@gmail.com>
2019-09-04 16:39:37 +0100

    Adding index.js file (Startup)

---
+++ Exercice_AccountBalance_ReactJs/AccountBalance_ReactJs/AccountBalance_ReactJs/src/index.js
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Account from './Account';
+import Edit from './Edit';
+
+ReactDOM.render(<Account />, document.getElementById('App'));
